a97a3230f434d692eb53b8ea252fce4f005251a4
Ruairidh Williamson <williamsonruairidh@gmail.com>
2024-11-30 19:32:11 +0000

    Support multisample

--- src/gfx.rs
+++ src/gfx.rs
@@ -1,7 +1,7 @@
 pub mod buffer;
 pub mod surface;
 
-use std::sync::Arc;
+use std::{num::NonZeroU32, sync::Arc};
 
 use buffer::GfxBuffer;
 use surface::GfxSurface;
@@ -37,6 +37,7 @@ pub enum GfxError {
 pub struct GfxConfig {
     pub present_mode: wgpu::PresentMode,
     pub required_features: wgpu::Features,
+    pub multisample_count: NonZeroU32,
 }
 
 impl Default for GfxConfig {
@@ -45,6 +46,7 @@ impl Default for GfxConfig {
             present_mode: wgpu::PresentMode::default(),
             required_features: wgpu::Features::TEXTURE_ADAPTER_SPECIFIC_FORMAT_FEATURES
                 | wgpu::Features::ADDRESS_MODE_CLAMP_TO_BORDER,
+            multisample_count: NonZeroU32::MIN,
         }
     }
 }
@@ -54,6 +56,8 @@ pub struct Gfx {
     pub device: wgpu::Device,
     pub queue: wgpu::Queue,
     pub config: wgpu::SurfaceConfiguration,
+    pub multisample_count: NonZeroU32,
+    pub multisample_view: Option<wgpu::TextureView>,
 }
 
 impl Gfx {
@@ -134,14 +138,9 @@ impl Gfx {
         size: PhysicalSize<u32>,
         config: &GfxConfig,
     ) -> Self {
-        let capabilities = {
-            let this = &internal;
-            match this {
-                GfxBacking::Surface(GfxSurface { surface, .. }) => {
-                    surface.get_capabilities(adapter)
-                }
-                GfxBacking::Buffer(_) => wgpu::SurfaceCapabilities::default(),
-            }
+        let capabilities = match &internal {
+            GfxBacking::Surface(GfxSurface { surface, .. }) => surface.get_capabilities(adapter),
+            GfxBacking::Buffer(_) => wgpu::SurfaceCapabilities::default(),
         };
         log::debug!("Found texture formats: {:?}", capabilities.formats);
         let texture_format = capabilities
@@ -162,7 +161,7 @@ impl Gfx {
         let sample_flags = adapter.get_texture_format_features(texture_format).flags;
         log::debug!("Sample flags {sample_flags:#?}");
 
-        let config = wgpu::SurfaceConfiguration {
+        let surface_config = wgpu::SurfaceConfiguration {
             usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
             format: texture_format,
             width: size.width,
@@ -173,28 +172,56 @@ impl Gfx {
             desired_maximum_frame_latency: 2,
         };
         if let GfxBacking::Surface(GfxSurface { surface, .. }) = &internal {
-            surface.configure(&device, &config);
+            surface.configure(&device, &surface_config);
         };
+
+        let multisample_view =
+            Self::create_multisample_view(&device, config.multisample_count, &surface_config);
 
         Self {
             backing: internal,
             device,
             queue,
-            config,
+            config: surface_config,
+            multisample_count: config.multisample_count,
+            multisample_view,
         }
     }
 
     pub fn get_current_texture(&self) -> Result<RenderableTexture, GfxError> {
-        {
-            let this = &self.backing;
-            match this {
-                GfxBacking::Surface(GfxSurface { surface, .. }) => {
-                    Ok(RenderableTexture::Surface(surface.get_current_texture()?))
-                }
-                GfxBacking::Buffer(buffer) => {
-                    Ok(RenderableTexture::Texture(Arc::clone(&buffer.texture)))
-                }
+        match &self.backing {
+            GfxBacking::Surface(GfxSurface { surface, .. }) => {
+                Ok(RenderableTexture::Surface(surface.get_current_texture()?))
             }
+            GfxBacking::Buffer(buffer) => {
+                Ok(RenderableTexture::Texture(Arc::clone(&buffer.texture)))
+            }
+        }
+    }
+
+    pub fn color_attachments<'a>(
+        &'a self,
+        load: wgpu::LoadOp<wgpu::Color>,
+        final_view: &'a wgpu::TextureView,
+    ) -> Result<wgpu::RenderPassColorAttachment<'a>, GfxError> {
+        if let Some(m) = &self.multisample_view {
+            Ok(wgpu::RenderPassColorAttachment {
+                view: m,
+                resolve_target: Some(final_view),
+                ops: wgpu::Operations {
+                    load,
+                    store: wgpu::StoreOp::Discard,
+                },
+            })
+        } else {
+            Ok(wgpu::RenderPassColorAttachment {
+                view: final_view,
+                resolve_target: None,
+                ops: wgpu::Operations {
+                    load,
+                    store: wgpu::StoreOp::Store,
+                },
+            })
         }
     }
 
@@ -227,11 +254,44 @@ impl Gfx {
     }
 
     pub fn window_resize(&mut self, size: &PhysicalSize<u32>) {
+        let old_size = (self.config.width, self.config.height);
         self.config.width = size.width;
         self.config.height = size.height;
+        let new_size = (self.config.width, self.config.height);
+        log::trace!("window resize {old_size:?} -> {new_size:?}");
         if let GfxBacking::Surface(GfxSurface { surface, .. }) = &self.backing {
             surface.configure(&self.device, &self.config);
         };
+        self.multisample_view =
+            Self::create_multisample_view(&self.device, self.multisample_count, &self.config);
+    }
+
+    fn create_multisample_view(
+        device: &wgpu::Device,
+        multisample_count: NonZeroU32,
+        config: &wgpu::SurfaceConfiguration,
+    ) -> Option<wgpu::TextureView> {
+        if multisample_count.get() > 1 {
+            let view = device
+                .create_texture(&wgpu::TextureDescriptor {
+                    label: None,
+                    size: wgpu::Extent3d {
+                        width: config.width,
+                        height: config.height,
+                        depth_or_array_layers: 1,
+                    },
+                    mip_level_count: 1,
+                    sample_count: multisample_count.get(),
+                    dimension: wgpu::TextureDimension::D2,
+                    format: config.format,
+                    usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+                    view_formats: &[],
+                })
+                .create_view(&wgpu::TextureViewDescriptor::default());
+            Some(view)
+        } else {
+            None
+        }
     }
 
     pub fn set_cursor_grab(&self, grab: bool) -> Result<(), GfxError> {
